4f1c9d12618c7449e51ad87c18ee00b14a1796a5
ndrohith09 <ndrohith09@gmail.com>
2021-11-21 12:25:21 +0530

    webpush

--- push.js
+++ push.js
@@ -1,9 +1,9 @@
 var push = require('web-push');
 // let vapidKeys = push.generateVAPIDKeys(); 
-let vapidKeys = {    
-        publicKey: 'BPMd2UB5GiddhZn1NjFRqCNs_WsCxaKLpkIhRpCXfqIwqbv3w3axBqKXV5p71fEOYIR-LFARcEBeB_UyNxqvvBg',
-        privateKey: '2-_dTYnUkNAebrZBfbSOetT3eV7BEUnprIRBLQTLPjI'   
-} 
+let vapidKeys = {
+    publicKey: 'BKOiRPfsD008p0P9mcKiPKBRtuGG4tinQp0plQaxL1VkwbOizhGC94nzOdLdKBEua-yAAmDhJ12iegPlpcARxNk',
+    privateKey: 'MxtLBVQ-iUhmx5H9A32sO2R377TdlRlHZ8_M9rKEAtE'
+  }
 console.log(vapidKeys);
 
 push.setVapidDetails('mailto:ndrohith09@gmail.com' , vapidKeys.publicKey , vapidKeys.privateKey);
